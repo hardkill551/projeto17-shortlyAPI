56cebbb2f44cdff6ab70ba2239a78bd7ef86092a
hardkill551 <hardkill194@gmail.com>
2023-05-22 16:44:13 -0300

    fix/idName

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -4,7 +4,7 @@ export async function getUserRepository (email) {
 	return db.query('SELECT * FROM users WHERE email = $1', [email]);
 }
 export async function getShortById (id) {
-	return db.query('SELECT urls.id AS "urlId", urls."shortUrl", urls."url", urls."visitCount" FROM urls JOIN users ON users.id = urls."userId" WHERE users.id = $1', [id]);
+	return db.query('SELECT urls.id, urls."shortUrl", urls."url", urls."visitCount" FROM urls JOIN users ON users.id = urls."userId" WHERE users.id = $1', [id]);
 }
 export async function getUserById (id) {
 	return db.query('SELECT users.id, users.name, SUM(urls."visitCount") AS "visitCount" FROM users JOIN urls ON users.id = urls."userId" WHERE users.id = $1 GROUP BY users.id', [id]);
